404d046106752f470f2c9e1efa3c3ce0cabdf15a
Cris Mihalache <cris.mihalache@spectrumit.eu>
2014-01-01 20:26:10 +0200

    Updated deploy script

--- fabfile.py
+++ fabfile.py
@@ -27,7 +27,6 @@ def _setup(branch):
     run("git pull origin master")
     run("git branch " + branch)
     run("git pull origin " + branch)
-    run("git submodule init")
 
     # Install global dependencies
     sudo("npm install -g codo grunt-cli forever")
@@ -50,11 +49,6 @@ def deploy():
     # Update
     run("git checkout production")
     run("git pull origin production")
-    run("git submodule update")
-
-    # Re-build line
-    run("cd line && npm install")
-    run("cd line && grunt")
 
     # Install any new modules
     run("npm install")
@@ -63,7 +57,7 @@ def deploy():
     run("grunt full")
 
     # Test (abort on fail)
-    #run("grunt test")
+    run("grunt test")
 
     # Deploy
     run("grunt deploy")
@@ -83,11 +77,6 @@ def stage():
     # Update
     run("git checkout staging")
     run("git pull origin staging")
-    run("git submodule update")
-
-    # Re-build line
-    run("cd line && npm install")
-    run("cd line && grunt")
 
     # Install any new modules
     run("npm install")
@@ -96,7 +85,7 @@ def stage():
     run("grunt full")
 
     # Test (abort on fail)
-    #run("grunt test")
+    run("grunt test")
 
     # Stage
     run("grunt stage")
