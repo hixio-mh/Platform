71946e6203eee073eddba525e488a3703b6f5b94
Cris Mihalache <cris.mihalache@spectrumit.eu>
2014-01-23 19:22:00 +0100

    Added Errorception, forgot to include newrelic.js

---
+++ newrelic.js
@@ -0,0 +1,13 @@
+exports.config = {
+  app_name: ["Adefy Platform"],
+  license_key: "3b6430e4d46f1ab7b98b92e199188fe3ffe75d26",
+  logging: {
+
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: "trace"
+  }
+};
