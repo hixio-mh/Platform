232685a1d12dd012e298004c0eb6728cf81dc686
Cris Mihalache <cris.mihalache@spectrumit.eu>
2014-03-13 11:49:36 +0100

    Added staging deploy, testing credentials to mongo setup script

--- provisioning/roles/api/files/setup_adefy.js
+++ provisioning/roles/api/files/setup_adefy.js
@@ -6,7 +6,12 @@ if(db.system.users.find().count() == 0) {
   db.addUser("adefy", "CdZcBjub3NRpoQNPhlrFQ6sZMgXI93DZGUE1CwkIgl8FjWCn");
 }
 
-use adefy_development
+use adefy_development;
 if(db.system.users.find().count() == 0) {
   db.addUser("adefy", "JPJsehsZkrBe15xIcQH419C2ZRoc4hg4uA90KVgjFkDbnKpQ");
+}
+
+use adefy_testing;
+if(db.system.users.find().count() == 0) {
+  db.addUser("adefy", "l3xzkDSBhGsAGhGpDWTqOX7NrQWyeNI3CXEfIiyMA2ckKbOn");
 }
