fdbf03df18b26f9fc8e40ea2ebb5791c7b9e6281
IceDragon200 <mistdragon100@gmail.com>
2014-03-22 11:43:13 -0500

    Updated the traffic generator
    1. id: > apikey:
    2. Added a log file
    3. Admin now does the serve requests
    4. Added click/impression stack size to console log

--- traffic.rb
+++ traffic.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/env ruby
 require 'adefy'
 
-agent = Adefy::Agent.new host: "http://www.adefy.dev/",
+@log = File.open("traffik.log", "w")
+@log.sync = true
+
+@admin_agent = Adefy::Agent.new host: "http://www.adefy.dev/",
                          apikey: "Nkv9tU54M9LLw9pSC8zIM8IB"
 
 ## admin
-agent.users.login username: "admin", password: "sachercake"
-pubs = agent.publishers.all
+@admin_agent.users.login username: "admin", password: "sachercake"
+@pubs = @admin_agent.publishers.all
 
 ## create a visitor agent
-agent_u = Adefy::Agent.new host: "http://www.adefy.dev/"
+#@agent_u = Adefy::Agent.new host: "http://www.adefy.dev/"
+
+# Use the admin agent for requests as well, I have no idea what will happen...
+@agent_u = @admin_agent
 
 i = 0
 loader = '|/-\\'
@@ -31,30 +37,31 @@ impressions_stack = []
 
 Thread.abort_on_exception = true
 
-def stack_thread(arra)
+def stack_thread(label, arra)
   Thread.new do
     loop do
       lnk = arra.shift
       begin
+        @log.puts "#{label} getting link #{lnk}"
         Excon.get(lnk) if lnk
         sleep 1.0
       rescue Excon::Errors::BadGateway
-        STDERR.puts "Clicks/Impressions Server appears to be down, waiting a few seconds"
+        @log.puts "Clicks/Impressions Server appears to be down, waiting a few seconds"
         sleep 3.0
       end
     end
   end
 end
 
-stack_thread(click_stack)
-stack_thread(impressions_stack)
+@click_thread = stack_thread("Click", click_stack)
+@impressions_thread = stack_thread("Impression", impressions_stack)
 
 loop do
   print line_clear
-  pub = pubs.sample
+  pub = @pubs.sample
 
   begin
-    hsh = agent_u.serve.serve(id: pub["apikey"])
+    hsh = @agent_u.serve.serve(apikey: pub["apikey"])
 
     imprs = hsh["impressionURL"]
     click = hsh["clickURL"]
@@ -63,12 +70,12 @@ loop do
     click_stack << click if click && rand < 0.5
 
   rescue Excon::Errors::BadGateway
-    STDERR.puts "Server appears to be down, waiting a few seconds"
+    @log.puts "Server appears to be down, waiting a few seconds"
     sleep 3.0
   rescue Excon::Errors::NotFound
     sleep 0.02
   end
-  print "Generating Traffic Like a Baws #{loader[i % loader.size]} : #{lyrics[(i / 5) % lyrics.size]}"
+  print "CLICK[#{"%03s" % click_stack.size}] IMPRESSION[#{"%03s" % impressions_stack.size}] Generating Traffic Like a Baws #{loader[i % loader.size]} : #{lyrics[(i / 5) % lyrics.size]}"
   i += 1
   sleep 0.20
 end
